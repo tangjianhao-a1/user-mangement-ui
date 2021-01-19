d89853d871f27c18a719a6148d793d182f5059f2
ede <837221025@qq.com>
2021-01-19 23:48:55 +0800

    项目重构

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <div class="table-head">
+      <input
+        type="text"
+        class="search"
+        placeholder="搜索用户"
+        v-model="search"
+      />
+      <button class="add">增加</button>
+    </div>
     <table sellspacing="0">
       <thead>
         <tr>
@@ -10,13 +19,13 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>李二猫</td>
-          <td>人不好</td>
+        <tr :key="item.id" v-for="item in userList">
+          <td>{{ item.id }}</td>
+          <td>{{ item.username }}</td>
+          <td>{{ item.noteInfo }}</td>
           <td>
-            <button>删除</button>  
-            <button>修改</button>
+            <button class="delete">删除</button>
+            <button class="edit">修改</button>
           </td>
         </tr>
       </tbody>
@@ -24,17 +33,29 @@
   </div>
 </template>
 
-<script>  
-import requestPath from '@/netwock/requestPath'
+<script>
+import requestPath from "@/netwock/requestPath";
 export default {
-  name: "home", 
+  name: "home",
   data() {
     return {
-      userlist: [],
+      search: "",
+      userList: [],
+    };
+  },
+
+  computed: {  
+    
+  },
+
+  async created() {
+    this.userList = this.Data()
+  },
+
+  methods: {
+   async Data() {
+     this.userList = await requestPath.getAllUser();
     }
-  },   
-async created() {
-    this.userlist = await requestPath.getAllUser()
   }
 };
 </script>
@@ -44,6 +65,43 @@ table,
 tr,
 td {
   border-collapse: collapse;
-  border: 1px solid red;
+  border: 1px solid #ebeb;
+  width: 50px;
+}
+
+table {
+  width: 800px;
+  height: 500px;
+  margin: -1px auto;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+th {
+  height: 40px;
+}
+
+.table-head {
+  height: 40px;
+  width: 798px;
+  border: 1px solid #ebeb;
+  margin: 0 auto;
+  margin-top: 100px;
+}
+
+.edit {
+  margin-left: 20px;
+}
+
+.search {
+  margin-left: 2px;
+  width: 590px;
+  height: 36px;
+}
+
+.add {
+  width: 60px;
+  height: 39px;
+  margin-left: 65px;
 }
 </style>
